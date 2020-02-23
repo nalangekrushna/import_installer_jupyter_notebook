40dc816ee851faa3139c5849bd7606508202e555
nalangekrushna <nalangekrushna@gmail.com>
2020-02-24 00:00:41 +0530

    populating correct list of packages needed to install

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-import ast
-import json
+import re, sys, json, warnings, pkgutil
+warnings.filterwarnings("ignore")
 
 data = ''
 with open('test.ipynb') as f :
@@ -8,13 +7,20 @@ with open('test.ipynb') as f :
 dic = json.loads(data)
 print(dic.keys())
 cells = dic['cells']
-import_list = []
+import_set = set()
 for cell in cells :
     if cell['cell_type'] == "code" :
         txt_list = cell['source']
         for txt in txt_list :
             if txt.startswith('from') :
-                import_list.append(re.search('from \w+',txt).group(0).split()[1])
+                import_set.add(re.search('from \w+',txt).group(0).split()[1])
             elif txt.startswith('import') :
-                import_list.append(re.search('import \w+',txt).group(0).split()[1])
-print('package list to install '+str(import_list))
+                import_set.add(re.search('import \w+',txt).group(0).split()[1])
+print('package list to import '+str(import_set))
+
+# alternative to below solution is this python library. stdlib-list
+lib_set = set([ key.split('.')[0] for key in sys.modules if not key.startswith('_') ])
+pkg_util_set = set([ i[1] for i in pkgutil.iter_modules(None) ])
+lib_set = lib_set | pkg_util_set
+install_set = import_set-lib_set
+print('package list to install '+str(install_set))
